d50602a6c8dc37dbab5940e69f17bb9d5fdbe56b
Nicholas Rutherford <nicholasrutherford@Nicholass-MBP.lan>
2025-05-21 22:45:41 -0500

    clean up gradle.kts file

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -51,27 +51,6 @@ dependencies {
     api(project(path = ":core:di"))
     api(project(path = ":compose:ui-theme"))
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-
-    implementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.navigation.compose)
-
-    testImplementation(libs.junit)
-    testImplementation(libs.koin.test)
-    testImplementation(libs.koin.test.junit4)
-
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.koin.android)
 }
